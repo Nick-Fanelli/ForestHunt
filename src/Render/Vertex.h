08275fe38216d76e5a8a87b8f96b7fafd3771fdf
Nick-Fanelli <nickfanelli5@gmail.com>
2021-11-26 09:46:20 -0500

    feat: add vertex struct

---
+++ src/Render/Vertex.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "pch.h"
+
+namespace ForestHunt {
+
+    struct Vertex {
+
+        glm::vec3 Position = { 0.0f, 0.0f, 0.0f };
+        glm::vec4 Tint = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+    };
+
+}
