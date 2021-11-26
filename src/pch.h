917c3be0cdd847e21f9d498a6df95549b1ae6ff7
Nick-Fanelli <nickfanelli5@gmail.com>
2021-11-25 21:46:21 -0500

    feat: add includes to pch

--- src/pch.h
+++ src/pch.h
@@ -1,2 +1,28 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <functional>
+#include <algorithm>
+#include <regex>
+#include <memory>
+#include <thread>
+#include <utility>
+
+#include <string>
+#include <stack>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+#include <filesystem>
+
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
